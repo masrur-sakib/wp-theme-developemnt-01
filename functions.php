7898d19b17b65552db83c636e9a4e8d4b7ae33e9
Masrur Sakib <mashrur333@gmail.com>
2020-07-24 12:52:10 +0600

    cache busting

--- functions.php
+++ functions.php
@@ -1,4 +1,11 @@
 <?php
+
+if(site_url()=="http://localhost/lwhh-test-01"){
+    define("VERSION", time());
+}else{
+    define("VERSION", wp_get_theme()->get("Version"));
+}
+
 function sakib_bootstrapping(){
     load_theme_textdomain("sakib");
     add_theme_support("post-thumbnails");
@@ -9,12 +16,12 @@ function sakib_bootstrapping(){
 add_action("after_setup_theme","sakib_bootstrapping");
 
 function sakib_assets(){
-    wp_enqueue_style("sakib", get_stylesheet_uri());
+    wp_enqueue_style("sakib", get_stylesheet_uri(), null, VERSION);
     wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
     wp_enqueue_style( "featherlight-css", "//cdn.jsdelivr.net/npm/featherlight@1.7.14/release/featherlight.min.css" );
-    wp_enqueue_script( "featherlight-js", "//cdn.jsdelivr.net/npm/featherlight@1.7.14/release/featherlight.min.js", array( "jquery" ), "0.0.1", true );
 
-    wp_enqueue_script( 'sakib-main', get_template_directory_uri()."/assets/js/main.js", array("jquery", "featherlight-js"), 1.1, true);
+    wp_enqueue_script( "featherlight-js", "//cdn.jsdelivr.net/npm/featherlight@1.7.14/release/featherlight.min.js", array( "jquery" ), "0.0.1", true );
+    wp_enqueue_script( 'sakib-main', get_template_directory_uri()."/assets/js/main.js", array("jquery", "featherlight-js"), VERSION, true);
 }
 add_action("wp_enqueue_scripts", "sakib_assets");
 
